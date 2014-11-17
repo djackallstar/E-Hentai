ed074b635f6d0986566a4528300115509e97e0c9
djackallstar <djackallstar@users.noreply.github.com>
2014-11-17 23:01:31 +0800

    Update Average Price Calculator for MoogleMail.user.js

--- Average Price Calculator for MoogleMail.user.js
+++ Average Price Calculator for MoogleMail.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name            Average Price Calculator for MoogleMail
-// @description     Shows the average price of items attached to a letter at the bottom of the body section
+// @name            Average Calculator for MoogleMail
+// @description     Calculates the average price of items attached to a letter
 // @include         http://hentaiverse.org/?s=Bazaar&ss=mm&filter=*
 // ==/UserScript==
 
