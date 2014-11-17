9e6f908cf31cde744bfc45a2682788d540f0ffc0
djackallstar <djackallstar@users.noreply.github.com>
2014-11-17 22:37:29 +0800

    Create Average Calculator for MoogleMail.user.js

---
+++ Average Calculator for MoogleMail.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name            Average Calculator for MoogleMail
+// @description     Calculates the average price of items attached to a letter
+// @include         http://hentaiverse.org/?s=Bazaar&ss=mm&filter=*
+// ==/UserScript==
+
+var wnd = window
+var doc = wnd.document
+var loc = location
+var href = loc.href
+
+var $  = function(e, css) { if(!css) { css=e; e=doc }; return e.querySelector(css) }
+var $$ = function(e, css) { if(!css) { css=e; e=doc }; return e.querySelectorAll(css) }
+
+var body_section = $('#leftpane').children[1]
+var s = body_section.textContent.replace(/>/g, '')
+var m = s.match(/\bOriginally attached item was: ([0-9]+)x ([^\(]+)\(.+, CoD was ([0-9]+)C/i)
+if(m) {
+    var quantity = parseInt(m[1])
+    var item = m[2].replace(/^ +| +$/g, '')
+    var cod = parseInt(m[3])
+    var avg_price = cod/quantity
+    body_section.innerHTML = body_section.innerHTML + '<HR>Item: ' + item + '<BR>Quantity: ' + quantity + '<BR>CoD: ' + cod + 'c<BR>Average Price: ' + avg_price + 'c'
+}
