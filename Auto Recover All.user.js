e94305dd73cdfef16a1213292a9852f513ece146
djackallstar <djackallstar@users.noreply.github.com>
2014-12-13 10:19:02 +0800

    Update Auto Recover All.user.js

--- Auto Recover All.user.js
+++ Auto Recover All.user.js
@@ -3,4 +3,13 @@
 // @description    This script automatically clicks the "Recover all" button when a battle has finished.
 // @include        http://hentaiverse.org/*
 // ==/UserScript==
-if(document.getElementById('togpane_log') && ((!document.getElementById('ckey_continue'))||(document.getElementById('ckey_continue').getAttribute('onclick')!='battle.battle_continue()')) && (document.getElementsByClassName('btcp'))){ recover_submit('all'); }
+
+var wnd = window
+var doc = wnd.document
+var loc = location
+var href = loc.href
+
+var $  = function(e, css) { if(!css) { css=e; e=doc }; return e.querySelector(css) }
+var $$ = function(e, css) { if(!css) { css=e; e=doc }; return e.querySelectorAll(css) }
+
+if($('#togpane_log') && ((!$('#ckey_continue')) || ($('#ckey_continue').getAttribute('onclick') != 'battle.battle_continue()')) && ($('.btcp'))) { recover_submit('all') }
