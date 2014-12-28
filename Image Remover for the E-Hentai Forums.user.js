f92e224295cb1427f33944996fa1024357a6c86d
djackallstar <djackallstar@users.noreply.github.com>
2014-12-28 14:08:28 +0800

    Create Image Remover for the E-Hentai Forums.user.js

---
+++ Image Remover for the E-Hentai Forums.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name            Image Remover for the E-Hentai Forums
+// @description     Removes specified images in the E-Hentai forums
+// @include         http://forums.e-hentai.org/*
+// ==/UserScript==
+
+var wnd = window
+var doc = wnd.document
+var loc = location
+var href = loc.href
+
+var $  = function(e, css) { if(!css) { css=e; e=doc }; return e.querySelector(css) }
+var $$ = function(e, css) { if(!css) { css=e; e=doc }; return e.querySelectorAll(css) }
+
+var p = [
+    /\bstyle_emoticons\//,
+]
+var imgs = $$('IMG')
+for(var i=imgs.length-1; i>=0; i--) {
+    for(var a=0, len=p.length; a<len; a++) {
+        if(p[a].test(imgs[i].src)) {
+            imgs[i].style.display = 'none'
+            break
+        }
+    }
+}
