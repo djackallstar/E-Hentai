7e2480ea7bff5bdc9e169243ad8dfe1dbf4b382c
djackallstar <djackallstar@users.noreply.github.com>
2015-03-16 19:02:55 +0800

    Update Average Price Calculator for MoogleMail.user.js

--- Average Price Calculator for MoogleMail.user.js
+++ Average Price Calculator for MoogleMail.user.js
@@ -12,16 +12,18 @@ var href = loc.href
 var $  = function(e, css) { if(!css) { css=e; e=doc }; return e.querySelector(css) }
 var $$ = function(e, css) { if(!css) { css=e; e=doc }; return e.querySelectorAll(css) }
 
-var body_section = $('#leftpane').children[1]
-var s = body_section.textContent.replace(/>/g, '')
-var m = s.match(/(?:Attached item removed: )(?:([0-9]+)x )?([^\(]+)\(.+, CoD was ([0-9]+)C/i)
-//var m = s.match(/(?:(?:Originally attached item was: )|(?:Attached item removed: ))(?:([0-9]+)x )?([^\(]+)\(.+, CoD was ([0-9]+)C/i)
-console.log(m)
-if(m) {
-    var quantity = parseInt(m[1])
-    if(isNaN(quantity)) { quantity = 1 }
-    var item = m[2].replace(/^ +| +$/g, '')
-    var cod = parseInt(m[3])
-    var avg_price = cod/quantity
-    body_section.innerHTML = body_section.innerHTML + '<HR>Item: ' + item + '<BR>Quantity: ' + quantity + '<BR>CoD: ' + cod + 'c<BR>Average Price: ' + avg_price + 'c'
+if(/&?\bmid=/.test(href)) {
+    var body_section = $('#leftpane').children[1]
+    var s = body_section.textContent.replace(/>/g, '')
+    var m = s.match(/(?:Attached item removed: )(?:([0-9]+)x )?([^\(]+)\(.+, CoD was ([0-9]+)C/i)
+    //var m = s.match(/(?:(?:Originally attached item was: )|(?:Attached item removed: ))(?:([0-9]+)x )?([^\(]+)\(.+, CoD was ([0-9]+)C/i)
+    console.log(m)
+    if(m) {
+        var quantity = parseInt(m[1])
+        if(isNaN(quantity)) { quantity = 1 }
+        var item = m[2].replace(/^ +| +$/g, '')
+        var cod = parseInt(m[3])
+        var avg_price = cod/quantity
+        body_section.innerHTML = body_section.innerHTML + '<HR>Item: ' + item + '<BR>Quantity: ' + quantity + '<BR>CoD: ' + cod + 'c<BR>Average Price: ' + avg_price + 'c'
+    }
 }
