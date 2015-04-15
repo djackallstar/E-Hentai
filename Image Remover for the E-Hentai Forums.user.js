9751b61848e20c0bb0c946f8ae43d9df03510763
djackallstar <djackallstar@users.noreply.github.com>
2015-04-15 13:34:42 +0800

    Update Image Remover for the E-Hentai Forums.user.js

--- Image Remover for the E-Hentai Forums.user.js
+++ Image Remover for the E-Hentai Forums.user.js
@@ -12,16 +12,19 @@ var href = loc.href
 var $  = function(e, css) { if(!css) { css=e; e=doc }; return e.querySelector(css) }
 var $$ = function(e, css) { if(!css) { css=e; e=doc }; return e.querySelectorAll(css) }
 
-var p = [
-    /\bstyle_emoticons\//,
-    /\/folder_post_icons\//,
-]
+
 var imgs = $$('IMG')
-for(var i=imgs.length-1; i>=0; i--) {
-    for(var a=0, len=p.length; a<len; a++) {
-        if(p[a].test(imgs[i].src)) {
-            imgs[i].style.display = 'none'
-            break
+if(imgs) {
+    var p = [
+        /\bstyle_emoticons\//,
+        /\/folder_post_icons\//,
+    ]
+    for(var i=imgs.length-1; i>=0; i--) {
+        for(var a=0, len=p.length; a<len; a++) {
+            if(p[a].test(imgs[i].src)) {
+                imgs[i].style.display = 'none'
+                break
+            }
         }
     }
 }
