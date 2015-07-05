ca4ed91b0c07be8f3c8b8e6f9316a15aeb2e7f2b
djackallstar <djackallstar@users.noreply.github.com>
2015-07-05 19:30:57 +0800

    Update HV - Open MoogleMail in New Tab.user.js

--- HV - Open MoogleMail in New Tab.user.js
+++ HV - Open MoogleMail in New Tab.user.js
@@ -9,7 +9,7 @@
 
 /*** Settings ***/
 
-var auto_take_attachment = true
+var auto_take_attachment = false
 var auto_recall = false // only used in the sentbox
 var auto_close_mail_without_attachment = false
 
@@ -30,23 +30,35 @@ for(var i=0, len=letters.length; i<len; i++) {
         if(/&filter=inbox/.test(href)) { url = 'http://hentaiverse.org/?s=Bazaar&ss=mm&filter=inbox&mid=' }
         else if(/&filter=sent/.test(href)) { url = 'http://hentaiverse.org/?s=Bazaar&ss=mm&filter=sent&mid=' }
         url += letters[i].onclick.toString().match(/&?\bmid=(\d+)/)[1]
-        letters[i].onclick = function() { GM_openInTab(url, true) }
+        letters[i].onclick = function() {
+            if(typeof GM_openInTab != 'undefined') { GM_openInTab(url, true) }
+            else { window.open(url, '_blank') }
+        }
     })()
 }
 if(auto_recall) {
     if(/&filter=sent/.test(href)) {
         if(/&?\bmid=/.test(href)) {
-            if($('img[src="http://ehgt.org/v/mooglemail/recallmail.png"]')) { unsafeWindow.mooglemail.return_mail() }
+            if($('img[src="http://ehgt.org/v/mooglemail/recallmail.png"]')) {
+                if(typeof unsafeWindow != 'undefined') { unsafeWindow.mooglemail.return_mail() }
+                else { window.mooglemail.return_mail() }
+            }
         }
     }
 }
 if(auto_take_attachment) {
     if(/&?\bmid=/.test(href)) {
-        if($('img[src="http://ehgt.org/v/mooglemail/takeattacheditem.png"]')) { unsafeWindow.mooglemail.remove_attachment() }
+        if($('img[src="http://ehgt.org/v/mooglemail/takeattacheditem.png"]')) {
+            if(typeof unsafeWindow != 'undefined') { unsafeWindow.mooglemail.remove_attachment() }
+            else { window.mooglemail.remove_attachment() }
+        }
     }
 }
 if(auto_close_mail_without_attachment) {
     if(/&?\bmid=/.test(href)) {
-        if(!$('img[src="http://ehgt.org/v/mooglemail/recallmail.png"]') && !$('img[src="http://ehgt.org/v/mooglemail/takeattacheditem.png"]') && $('img[src="http://ehgt.org/v/mooglemail/closemail.png"]')) { unsafeWindow.close() }
+        if(!$('img[src="http://ehgt.org/v/mooglemail/recallmail.png"]') && !$('img[src="http://ehgt.org/v/mooglemail/takeattacheditem.png"]') && $('img[src="http://ehgt.org/v/mooglemail/closemail.png"]')) {
+            if(typeof unsafeWindow != 'undefined') { unsafeWindow.close() }
+            else { window.close() }
+        }
     }
 }
