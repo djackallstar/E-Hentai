b6c99b0de7ae616f8aa693987712bbf1e5ffd0a5
djackallstar <djackallstar@users.noreply.github.com>
2015-07-05 18:56:20 +0800

    Create HV - Open MoogleMail in New Tab.user.js

---
+++ HV - Open MoogleMail in New Tab.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name        HV - Open MoogleMail in New Tab
+// @updateURL   about:blank
+// @grant       unsafeWindow
+// @grant       GM_openInTab
+// @include     http://hentaiverse.org/?s=Bazaar&ss=mm&filter=inbox*
+// @include     http://hentaiverse.org/?s=Bazaar&ss=mm&filter=sent*
+// ==/UserScript==
+
+/*** Settings ***/
+
+var auto_take_attachment = true
+var auto_recall = false // only used in the sentbox
+
+/*** End of Settings ***/
+
+var wnd = window
+var doc = wnd.document
+var loc = location
+var href = loc.href
+
+var $  = function(e, css) { if(!css) { css=e; e=doc }; return e.querySelector(css) }
+var $$ = function(e, css) { if(!css) { css=e; e=doc }; return e.querySelectorAll(css) }
+
+var letters = $('#mainpane').querySelectorAll('tr[onclick]')
+for(var i=0, len=letters.length; i<len; i++) {
+    (function() {
+        var url = ''
+        if(/&filter=inbox/.test(href)) { url = 'http://hentaiverse.org/?s=Bazaar&ss=mm&filter=inbox&mid=' }
+        else if(/&filter=sent/.test(href)) { url = 'http://hentaiverse.org/?s=Bazaar&ss=mm&filter=sent&mid=' }
+        url += letters[i].onclick.toString().match(/&?\bmid=(\d+)/)[1]
+        letters[i].onclick = function() { GM_openInTab(url, true) }
+    })()
+}
+if(auto_recall) {
+    if(/&filter=sent/.test(href)) {
+        if(/&?\bmid=/.test(href)) {
+            if($('img[src="http://ehgt.org/v/mooglemail/recallmail.png"]')) { unsafeWindow.mooglemail.return_mail() }
+        }
+    }
+}
+if(auto_take_attachment) {
+    if(/&?\bmid=/.test(href)) {
+        if($('img[src="http://ehgt.org/v/mooglemail/takeattacheditem.png"]')) { unsafeWindow.mooglemail.remove_attachment() }
+    }
+}
