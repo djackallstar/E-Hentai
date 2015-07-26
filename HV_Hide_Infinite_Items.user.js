8944674b843881c43fad89ec09a9c2b14159a27a
djackallstar <djackallstar@users.noreply.github.com>
2015-07-26 17:37:44 +0800

    Update HV_Hide_Infinite_Items.user.js

--- HV_Hide_Infinite_Items.user.js
+++ HV_Hide_Infinite_Items.user.js
@@ -5,14 +5,24 @@
 // ==/UserScript==
 
 /*** Settings ***/
+if(typeof wlist == 'undefined') {
+    var wlist = [ // Infinite items on the list are NOT hidden. Edit it to suit your own needs.
+        /Soul Fragment/i,
+        /Scrap (Cloth|Wood)/i,
+        /Energy Cell/i,
+        /(Regular|Robust|Vibrant|Coruscating) Catalyst/i,
+    ]
+}
+
 if(typeof blist == 'undefined') {
-    var blist = [ // Items on the list are hidden. Edit it to suit your own needs.
+    var blist = [ // Finite items on the list are hidden. Edit it to suit your own needs.
         /Featherweight|Voidseeker/i,
         /(Low|Mid)-Grade/i,
         /High-Grade (Metals|Leather)/i,
         /Scroll of (Swiftness|Shadows|Absorption|Life|(the Gods))/i,
     ]
 }
+
 /*** End of Settings ***/
 
 var wnd = window
@@ -29,10 +39,14 @@ if(/&ss=is&?/.test(href)) {
         var items = $$(shop_pane, '.idp')
         var supplies = $$(shop_pane, '.ii')
         for(var i=0, len=items.length; i<len; i++) {
-            if(isNaN(parseInt(supplies[i].textContent))) {
-                items[i].parentNode.parentNode.style.display = 'none'
+            if(isNaN(parseInt(supplies[i].textContent))) { // Items with infinite stock
+                for(var j=0, len2=wlist.length; j<len2; j++) {
+                    if(!wlist[j].test(items[i].textContent)) {
+                        items[i].parentNode.parentNode.style.display = 'none'
+                    }
+                }
             }
-            for(var j=0, len2=blist.length; j<len2; j++) {
+            for(var j=0, len2=blist.length; j<len2; j++) { // Items with finite stock
                 if(blist[j].test(items[i].textContent)) {
                     items[i].parentNode.parentNode.style.display = 'none'
                 }
