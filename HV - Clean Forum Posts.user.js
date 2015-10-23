d89448a5ca77677ccc7428c1a39fceebd5c99c96
djackallstar <djackallstar@users.noreply.github.com>
2015-10-23 11:30:02 +0800

    Update HV - Clean Forum Posts.user.js

--- HV - Clean Forum Posts.user.js
+++ HV - Clean Forum Posts.user.js
@@ -1,10 +1,13 @@
 // ==UserScript==
 // @name        HV - Clean Forum Posts
 // @updateURL   about:blank
+// @grant       unsafeWindow
 // @include     http://forums.e-hentai.org/index.php?showtopic=*
+// @include     http://act=Search&CODE=show*
+// @include     /^https?://forums\.e-hentai\.org/index\.php\?.*result_type=posts/
 // ==/UserScript==
 
-var wnd = window
+var wnd = unsafeWindow
 var doc = wnd.document
 var loc = location
 var href = loc.href
@@ -13,27 +16,29 @@ var $  = function(e, css) { if(!css) { css=e; e=doc }; return e.querySelector(cs
 var $$ = function(e, css) { if(!css) { css=e; e=doc }; return e.querySelectorAll(css) }
 
 /*** Settings ***/
-if(typeof uid_blist == 'undefined') {
+if(typeof wnd.uid_blist == 'undefined') {
     var uid_blist = [ // hide posts by uid (integer)
-        -1,
+        -12345,
     ]
-}
-if(typeof uname_blist == 'undefined') {
+} else { var uid_blist = wnd.uid_blist }
+if(typeof wnd.uname_blist == 'undefined') {
     var uname_blist = [ // hide quotes by username (string)
         '',
     ]
-}
+} else { var uname_blist = wnd.uname_blist }
 var hide_cutie_marks = true
-var hide_warn_level = true
+var hide_warn_levels = true
 
 /*** End of Settings ***/
 
+// Hide posts
 var borderwrap = $$('.borderwrap')
 for(var i=0, len=borderwrap.length; i<len; i++) {
     var is_post = ($(borderwrap[i], '.postcolor') != null)
     if(is_post) {
-        var uid = $(borderwrap[i], '.post1 a[href^="http://forums.e-hentai.org/index.php?showuser="]')
-        if(!uid) { uid = $(borderwrap[i], '.post2 a[href^="http://forums.e-hentai.org/index.php?showuser="]') }
+        var uid = $(borderwrap[i], 'a[href^="http://forums.e-hentai.org/index.php?showuser="]')
+        //var uid = $(borderwrap[i], '.post1 a[href^="http://forums.e-hentai.org/index.php?showuser="]')
+        //if(!uid) { uid = $(borderwrap[i], '.post2 a[href^="http://forums.e-hentai.org/index.php?showuser="]') }
         if(uid) {
             uid = parseInt(uid.href.match(/\?showuser=(\d+)/)[1])
             if(uid_blist.indexOf(uid) != -1) {
@@ -45,6 +50,7 @@ for(var i=0, len=borderwrap.length; i<len; i++) {
     }
 }
 
+// Hide quotes
 var p = new RegExp('^QUOTE\\((' + uname_blist.join('|') + ') @', 'i')
 var postcolor = $$('.postcolor')
 for(var i=0, len=postcolor.length; i<len; i++) {
@@ -61,12 +67,14 @@ for(var i=0, len=postcolor.length; i<len; i++) {
     }
 }
 
+// Hide cutie marks
 if(hide_cutie_marks) {
     var div = $$('DIV[style*="url(http://forums.e-hentai.org/ehgt/cm/"]')
     for(var i=0, len=div.length; i<len; i++) { div[i].style.display = 'none' }
 }
 
-if(hide_warn_level) {
+// Hide warn levels
+if(hide_warn_levels) {
     var w = $$('img[src*="style_images/ambience/warn"]')
     for(var i=0, len=w.length; i<len; i++) {
         w[i].style.display = 'none'
