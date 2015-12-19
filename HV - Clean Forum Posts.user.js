7de29f0491d4881aac51b3bb97bbd5339120bef5
djackallstar <djackallstar@users.noreply.github.com>
2015-12-19 23:52:54 +0800

    Update HV - Clean Forum Posts.user.js

--- HV - Clean Forum Posts.user.js
+++ HV - Clean Forum Posts.user.js
@@ -19,26 +19,186 @@ var $  = function(e, css) { if(!css) { css=e; e=doc }; return e.querySelector(cs
 var $$ = function(e, css) { if(!css) { css=e; e=doc }; return e.querySelectorAll(css) }
 
 /*** Settings ***/
-if(typeof wnd.uid_blist == 'undefined') {
-    var uid_blist = [ // hide posts by uid (integer)
-        0,
+
+// For forums
+var hide_pinned_threads = true
+var hide_unread_imgs = true
+var hide_closed_shops = true
+var highlight_auctions = true
+var highlight_shop_owners = true
+var hide_bottom_area = true
+var hide_last_posts = true
+if(typeof do_not_hide == 'undefined') {
+    var do_not_hide = [ // user IDs
+        2328, // atomicpuppy
+        409722, // danixxx
+        1988471,
     ]
-} else { var uid_blist = wnd.uid_blist }
+}
+if(typeof wts_shop_owners_to_highlight == 'undefined') {
+    var wts_shop_owners_to_highlight = [ // user IDs
+        1988471,
+    ]
+}
+if(typeof wtb_shop_owners_to_highlight == 'undefined') {
+    var wtb_shop_owners_to_highlight = [ // user IDs
+        1988471,
+    ]
+}
+if(typeof wts_thread_blist == 'undefined') {
+    var wts_thread_blist = [ // thread IDs
+        163637, // The Shared Free Shop
+        183055,
+    ]
+}
+if(typeof wtb_thread_blist == 'undefined') {
+    var wtb_thread_blist = [ // thread IDs
+    ]
+}
+if(typeof chat_thread_wlist == 'undefined') {
+    var chat_thread_wlist = [ // thread IDs
+        178483, // The Legendary Equipment Thread
+        65126, // Script Thread
+    ]
+}
+if(typeof user_blist == 'undefined') {
+    var user_blist = [ // user IDs
+    ]
+}
 if(typeof wnd.uname_blist == 'undefined') {
     var uname_blist = [ // hide quotes by username (string)
         '',
     ]
 } else { var uname_blist = wnd.uname_blist }
-var dont_clean = [ // topic id
-    169987,
-]
+
+// For posts
 var hide_cutie_marks = true
 var hide_post_count = true
 var hide_warn_levels = true
-var hide_bottom_area = true
-var hide_last_posts = true
+if(typeof wnd.uid_blist == 'undefined') {
+    var uid_blist = [ // hide posts by uid (integer)
+        0,
+    ]
+} else { var uid_blist = wnd.uid_blist }
+var dont_clean = [ // topic id
+    169987,
+]
 
 /*** End of Settings ***/
+
+/*** The BWList script ***/
+
+var showforum = href.match(/showforum=(\d+)/)
+if(!showforum) { showforum = href.match(/&?\bf=(\d+)/) }
+if(showforum) {
+    showforum = showforum[1]
+
+    // Hide unread imgs
+    if(hide_unread_imgs) {
+        var imgs = $$('img[src="style_images/ambience/newpost.gif"]')
+        for(var i=0, len=imgs.length; i<len; i++) { imgs[i].style.display = 'none' }
+        var imgs = $$('img[src="style_images/fusion/newpost.gif"]')
+        for(var i=0, len=imgs.length; i<len; i++) { imgs[i].style.display = 'none' }
+    }
+
+    // Hide closed shops
+    if(hide_closed_shops) {
+        if(showforum == '77' || showforum == '78') {
+            var lnks = $$('td.row1 > div > span > a[id^="tid-link-"]')
+            //var closed_patterns = /\b(close|closed|complete|(delete|del\b)|done|end|none|nothing|shut|sorry)/i
+            var closed_patterns = /\b(close|closed|complete|(delete|del\b)|done|end|none|nothing|shut|sorry|auction|lottery|lotto)/i
+            for(var i=0, len=lnks.length; i<len; i++) {
+                if((!/&st=/.test(href)) || (/&st=0/.test(href))) { // (probably) on the first page
+                    if(/\b(auction|lottery|lotto)\b/i.test(lnks[i].textContent)) { continue }
+                }
+                var owner = $(lnks[i].parentNode.parentNode.parentNode.parentNode, 'td.row2 > a[href*="showuser"]')
+                if(do_not_hide.indexOf(parseInt(owner.href.match(/showuser=(\d+)/)[1])) != -1){ continue }
+                if(closed_patterns.test(lnks[i].textContent)) {
+                    lnks[i].parentNode.parentNode.parentNode.parentNode.style.display = 'none'
+                    continue
+                }
+                var desc = $(lnks[i].parentNode.parentNode, 'span#tid-desc-'+lnks[i].id.match(/(\d+)/)[1])
+                if(closed_patterns.test(desc.textContent)) {
+                    desc.parentNode.parentNode.parentNode.parentNode.style.display = 'none'
+                    continue
+                }
+            }
+        }
+    }
+
+    // Highlight auctions
+    if(highlight_auctions) {
+        if(showforum == '77') {
+            var lnks = $$('td.row1 > div > span > a[id^="tid-link-"]')
+            for(var i=0, len=lnks.length; i<len; i++) {
+                if(/\b(auction|lottery|lotto)\b/i.test(lnks[i].textContent)) {
+                    var owner = $(lnks[i].parentNode.parentNode.parentNode.parentNode, 'td.row2 > a[href*="showuser"]')
+                    if(owner.style.color == '') { owner.style.color = 'blue'}
+                }
+            }
+        }
+    }
+
+    // Highlight shop owners
+    if(highlight_shop_owners) {
+        var fav = []
+        if(showforum == '77') { fav = wts_shop_owners_to_highlight } // WTS
+        else if(showforum == '78') { fav = wtb_shop_owners_to_highlight } // WTB
+        var owner = $$('td.row2 > a[href*="showuser"]')
+        for(var i=0, len=owner.length; i<len; i++) {
+            //if((fav.indexOf(owner[i].textContent) != -1) && (owner[i].style.color == '')) { owner[i].style.color = 'red' }
+            for(var j=0, len_j=fav.length; j<len_j; j++) {
+                if((new RegExp('showuser='+fav[j]+'\\b').test(owner[i].href)) && (owner[i].style.color == '')) {
+                    owner[i].style.color = 'red'
+                    break
+                }
+            }
+        }
+    }
+
+    // Hide specified forum threads
+    var thread_blist = thread_wlist = []
+    if(showforum == '76')      { thread_wlist = chat_thread_wlist } // Chat
+    else if(showforum == '77') { thread_blist = wts_thread_blist } // WTS
+    else if(showforum == '78') { thread_blist = wtb_thread_blist } // WTB
+    if(thread_blist.length) {
+        for(var i=0, len=thread_blist.length; i<len; i++) {
+            try {
+                $('#tid-link-'+thread_blist[i]).parentNode.parentNode.parentNode.parentNode.style.display = 'none'
+            } catch(e) {}
+        }
+    }
+    else if(thread_wlist.length) {
+        var lnks = $$('td.row1 > div > span > a[id^="tid-link-"]')
+        for(var i=0, len=lnks.length; i<len; i++) {
+            if(!/^tid-link-/.test(lnks[i].id)) { continue }
+            var tid = parseInt(lnks[i].id.match(/tid-link-(\d+)/)[1])
+            if(thread_wlist.indexOf(tid)==-1) {
+                try {
+                    lnks[i].parentNode.parentNode.parentNode.parentNode.style.display = 'none'
+                } catch(e) {}
+            }
+        }
+    }
+
+    // Hide threads made by specific users
+    if(user_blist.length) {
+        var users = $$('td.row2 > a[href*="showuser"]')
+        for(var i=0, len=users.length; i<len; i++) {
+            for(var j=0, len_j=user_blist.length; j<len_j; j++) {
+                //if((new RegExp('showuser='+user_blist[j]+'\\b').test(users[i].href)) && (users[i].style.color == '')) {
+                if(new RegExp('showuser='+user_blist[j]+'\\b').test(users[i].href)) {
+                    if(!/\bauction\b/i.test(users[i].parentNode.parentNode.querySelectorAll('.row1')[2].textContent)) {
+                        users[i].parentNode.parentNode.style.display = 'none'
+                    }
+                    break
+                }
+            }
+        }
+    }
+}
+
+/*** End of the BWList script ***/
 
 if(/act=post/i.test(href)) {
     var ids = $$('.row2:first-child')
@@ -51,23 +211,33 @@ if(/act=post/i.test(href)) {
     }
 }
 else if(/:\/\/forums\./.test(href)) {
-    // Hide posts
-    var hide_this = true
-    if(/showtopic=\d+/.test(href) && (dont_clean.indexOf(parseInt(href.match(/showtopic=(\d+)/)[1])) != -1)) { hide_this = false }
-    if(hide_this) {
-        var borderwrap = $$('.borderwrap')
-        for(var i=0, len=borderwrap.length; i<len; i++) {
-            var is_post = ($(borderwrap[i], '.postcolor') != null)
-            if(is_post) {
-                var uid = $(borderwrap[i], 'a[href^="http://forums.e-hentai.org/index.php?showuser="], a[href^="https://forums.e-hentai.org/index.php?showuser="]')
-                if(uid) {
-                    uid = parseInt(uid.href.match(/\?showuser=(\d+)/)[1])
-                    if(uid_blist.indexOf(uid) != -1) {
-                        borderwrap[i].style.display = 'none'
-                        try { borderwrap[i].previousSibling.previousSibling.style.display = 'none' } catch(e) {}
-                        try { borderwrap[i].nextSibling.nextSibling.style.display = 'none' } catch(e) {}
+    if(/showtopic=\d+/.test(href)) {
+        // Hide posts
+        var hide_this = true
+        if(/showtopic=\d+/.test(href) && (dont_clean.indexOf(parseInt(href.match(/showtopic=(\d+)/)[1])) != -1)) { hide_this = false }
+        if(hide_this) {
+            var borderwrap = $$('.borderwrap')
+            for(var i=0, len=borderwrap.length; i<len; i++) {
+                var is_post = ($(borderwrap[i], '.postcolor') != null)
+                if(is_post) {
+                    var uid = $(borderwrap[i], 'a[href^="http://forums.e-hentai.org/index.php?showuser="], a[href^="https://forums.e-hentai.org/index.php?showuser="]')
+                    if(uid) {
+                        uid = parseInt(uid.href.match(/\?showuser=(\d+)/)[1])
+                        if(uid_blist.indexOf(uid) != -1) {
+                            borderwrap[i].style.display = 'none'
+                            try { borderwrap[i].previousSibling.previousSibling.style.display = 'none' } catch(e) {}
+                            try { borderwrap[i].nextSibling.nextSibling.style.display = 'none' } catch(e) {}
+                        }
                     }
                 }
+            }
+        }
+    }
+    else if(/showforum=\d+/.test(href)) {
+        if(hide_pinned_threads) {
+            var divs = document.querySelectorAll('tr>td.row1>div')
+            if(divs) {
+                for(var i=divs.length-1; i>=0; i--) { if(/Pinned:/.test(divs[i].textContent)) { divs[i].parentNode.parentNode.style.display = 'none' } }
             }
         }
     }
