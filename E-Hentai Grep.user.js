e7c8d9541fda073f5085f52de0e55c92849b6237
djackallstar <djackallstar@users.noreply.github.com>
2016-02-03 21:40:53 +0800

    Update E-Hentai Grep.user.js

--- E-Hentai Grep.user.js
+++ E-Hentai Grep.user.js
@@ -78,7 +78,7 @@ if(typeof grep_patterns == 'undefined') {
         [ // Holy
             [
                 /(Peer|Leg).*Frugal.*Heimdall/i,
-                /(Peer|Leg).*Frugal.*Heaven-sent/i,
+                /(Peer|Leg).*Frugal.*(Heaven-sent|Curse-weaver)/i,
             ], 'darkred', ''
         ],
     ]
