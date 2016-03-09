0753aa9fadf5f18334910c0060e23a3418f776f0
djackallstar <djackallstar@users.noreply.github.com>
2016-03-09 13:27:28 +0800

    Update Average Price Calculator for MoogleMail.user.js

--- Average Price Calculator for MoogleMail.user.js
+++ Average Price Calculator for MoogleMail.user.js
@@ -1,6 +1,7 @@
 // ==UserScript==
-// @name            Average Price Calculator for MoogleMail
-// @description     Shows the average price of items attached to a letter at the bottom of the body section.
+// @name            HV - Average Price Calculator for MoogleMail
+// @updateURL       about:blank
+// @grant           none
 // @include         http://hentaiverse.org/?s=Bazaar&ss=mm&filter=*
 // ==/UserScript==
 
@@ -13,17 +14,26 @@ var $  = function(e, css) { if(!css) { css=e; e=doc }; return e.querySelector(cs
 var $$ = function(e, css) { if(!css) { css=e; e=doc }; return e.querySelectorAll(css) }
 
 if(/&mid=/.test(href)) {
-    var body_section = $('#leftpane').children[1]
-    var s = body_section.textContent.replace(/>/g, '')
-    var m = s.match(/(?:Attached item removed: )(?:([0-9]+)x )?([^\(]+)\(.+, CoD was ([0-9]+)C/i)
-    //var m = s.match(/(?:(?:Originally attached item was: )|(?:Attached item removed: ))(?:([0-9]+)x )?([^\(]+)\(.+, CoD was ([0-9]+)C/i)
-    console.log(m)
-    if(m) {
-        var quantity = parseInt(m[1])
-        if(isNaN(quantity)) { quantity = 1 }
-        var item = m[2].replace(/^ +| +$/g, '')
-        var cod = parseInt(m[3])
-        var avg_price = cod/quantity
-        body_section.innerHTML = body_section.innerHTML + '<HR>Item: ' + item + '<BR>Quantity: ' + quantity + '<BR>CoD: ' + cod + 'c<BR>Average Price: ' + avg_price + 'c'
+    var m = $('#leftpane').nextSibling.nextSibling.nextSibling
+    if(m.querySelector('*[onmouseover]')) {
+        var cnt = parseInt(m.children[0].textContent.match(/Attached:\s*(\d+)x/)[1])
+        var name = m.children[1].textContent.replace(/\n/g, '')
+        var cod = m.children[2].textContent.match(/CoD:\s*([0-9,]+)\s*credits/i)
+        if(cod) { cod = parseInt(cod[1].replace(/,/g, '')) } else { cod = 0 }
+        var avg_price = cod/cnt
+        $('#leftpane').children[1].innerHTML += '<HR>Name: ' + name + '<BR>Count: ' + cnt + '<BR>CoD: ' + cod + '<BR>Average Price: ' + avg_price
+    }
+    else {
+        var body_section = $('#leftpane').children[1]
+        var s = body_section.textContent.replace(/>/g, '')
+        var m = s.match(/(?:Attached item removed: )(?:([0-9]+)x )?([^\(]+)\(.+, CoD was ([0-9]+)C/i)
+        if(m) {
+            var cnt = parseInt(m[1])
+            if(isNaN(cnt)) { cnt = 1 }
+            var name = m[2].replace(/^ +| +$/g, '')
+            var cod = parseInt(m[3])
+            var avg_price = cod/cnt
+            body_section.innerHTML = body_section.innerHTML + '<HR>Name: ' + name + '<BR>Count: ' + cnt + '<BR>CoD: ' + cod + '<BR>Average Price: ' + avg_price
+        }
     }
 }
