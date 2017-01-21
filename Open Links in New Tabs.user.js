8cfc48cd7c694f39c1ba25ea65718723d256ef8c
djackallstar <djackallstar@users.noreply.github.com>
2017-01-21 22:16:54 +0800

    Update Open Links in New Tabs.user.js

--- Open Links in New Tabs.user.js
+++ Open Links in New Tabs.user.js
@@ -2,10 +2,12 @@
 // @name            Open Links in New Tabs
 // @description     Change the target attribute of specific anchor object to '_blank'
 // @updateURL       about:blank
-// @include         http://g.e-hentai.org/*
-// @include         https://g.e-hentai.org/*
 // @include         http://exhentai.org/*
 // @include         https://exhentai.org/*
+// @include         http://e-hentai.org/*
+// @include         https://e-hentai.org/*
+// @include         http://g.e-hentai.org/*
+// @include         https://g.e-hentai.org/*
 // ==/UserScript==
 
 var wnd = window
@@ -16,7 +18,7 @@ var href = loc.href
 var $  = function(e, css) { if(!css) { css=e; e=doc }; return e.querySelector(css) }
 var $$ = function(e, css) { if(!css) { css=e; e=doc }; return e.querySelectorAll(css) }
 
-if(/^https?:\/\/((g\.)|(ex))/.test(href)) {
+if(/^https?:\/\/((ex)|(e-)|(g\.))/.test(href)) {
     var lnks = $$('.itg a')
     if(lnks) { for(var i=lnks.length-1; i>=0; i--) { lnks[i].target = '_blank' } }
     var lnks = $$('#gdt a')
